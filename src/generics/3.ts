cb04373103c96814004d8a9b485c7ccf08264cf4
Nadija Smerechuk <nadija1smerechuk@gmail.com>
2024-10-05 20:36:23 +0300

    fixed task3

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -3,7 +3,7 @@
 //     що ці об'єкти можуть бути будь-якого типу.
 
 function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
-  return Object.assign(objA, objB);
+  return Object.assign({}, objA, objB);
 }
 
 const merged = merge({ name: 'Alisa' }, { age: 28 });
